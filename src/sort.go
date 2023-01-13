615e09f4cc0caacacd6dd195a7ed5c769abbe863
Ajinkya Bokade <ajinkyabokade@Ajinkyas-MBP.lan>
2023-01-12 22:24:46 -0800

    cleanup

--- src/sort.go
+++ src/sort.go
@@ -25,38 +25,19 @@ func main() {
 	// Read input file name
 	inputFileName := os.Args[1]
 
-	// Read input file
+	// Open input file
 	inputFile, err := os.Open(inputFileName)
 
 	if err != nil {
 		log.Fatalf("Error in opening input file - %v", err)
 	}
-	// data, err := ioutil.ReadFile(inputFileName)
-
-	// if err != nil {
-	// 	log.Fatalf("Error in reading input file - %v", err)
-	// }
 
 	// Declare records array which will store individual record
 	records := []record{}
 	for {
-		// var key [10]byte
-		// // read first 10 bytes and copy it into key
-		// copy(key[:], data[:10])
-		// // remove first 10 bytes from data
-		// data = data[10:]
-		// var value [90]byte
-		// // read first 90 bytes and copy it into value
-		// copy(value[:], data[:90])
-		// // remove first 90 bytes from data
-		// data = data[90:]
-		// // Create record object with key and value
-		// rec := record{key, value}
-		// // Append record to the records array
-		// records = append(records, rec)
-
 		var key [10]byte
 		var value [90]byte
+		// Read first 10 bytes into key
 		_, err := inputFile.Read(key[:])
 		if err != nil {
 			if err == io.EOF {
@@ -64,6 +45,7 @@ func main() {
 			}
 			log.Println(err)
 		}
+		// Read the next 90 bytes into value
 		_, err = inputFile.Read(value[:])
 		if err != nil {
 			if err == io.EOF {
@@ -96,7 +78,12 @@ func main() {
 	}
 	// Writing records to the output file
 	for _, rec := range records {
+		// Write key into the file
 		_, err = outputFile.Write(rec.Key[:])
+		if err != nil {
+			log.Println(err)
+		}
+		// Write value into the file
 		_, err = outputFile.Write(rec.Value[:])
 		if err != nil {
 			log.Println(err)
